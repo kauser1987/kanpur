5f2c24cb2ffd6648b43d494131f6c087979624f1
kauser1987 <kauser1987@gmail.com>
2024-02-19 10:02:51 +0530

    some features added

--- script/script1.js
+++ script/script1.js
@@ -15,9 +15,32 @@ const scroll = new LocomotiveScroll({
         
     }, 150);
  }
- timer()
  gsap.to("#page1",{
     top:"-100%",
     delay:2,
     duration:3,
  })
+ 
+ function mousefollower(){
+    var circle= document.querySelector("#minicircle");
+    window.addEventListener("mousemove",(dets)=>{
+        circle.style.transform=`translate(${dets.clientX}px,${dets.clientY}px)`
+    })
+}
+// function pressedfollower() {
+//     var scaleX;
+//     var scaleY;
+//     var xdiff;       
+//     var ydiff;
+//     scaleX=gsap.utils.clamp(.8,1.2,dets.clientX-xdiff)
+//     scaleY=gsap.utils.clamp(.8,1.2,dets.clientY-ydiff)
+//     xdiff=dets.clientX;
+//     ydiff=dets.clientY;
+//     mousefollower(scaleX,scaleY)
+//     window.addEventListener("mousemove",(dets)=>{
+//         circle.style.transform=`translate(${dets.clientX}px,${dets.clientY}px) scale(${2},${2})`
+//     })    
+// }
+// pressedfollower()
+mousefollower()
+timer()
---
+++ script/script2.js
@@ -0,0 +1,6 @@
+gsap.to("#page2 #label h1",{
+    y:0,
+    delay:3.5,
+    duration:1,
+    
+})
